7d29c65270d1211885bd2673530d8b9864213ad7
AlxFL <alexander@grubbe.co>
2016-12-16 18:57:08 -0500

    Interaction exercise

--- src/pages/interaction-exercise/interaction-exercise.ts
+++ src/pages/interaction-exercise/interaction-exercise.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NavController, NavParams, Slides } from 'ionic-angular';
 
 declare var firebase : any
 
@@ -10,14 +10,54 @@ declare var firebase : any
 
 export class InteractionExercise implements OnInit {
 
+
+  @ViewChild('sliderComprehension') slider: Slides;
+
+  mySlideOptions = {
+    slidesPerView: 2,
+    centeredSlides: true,
+    spaceBetween : 200
+  };
+
   title : any // Titulo del header
   exerciseNum : any
   pageTitle : any // Titulo del ejercicio
   description : any // Descripción del ejercicio
 
+  // CONTENIDOS POR PANTALLAS
+  ex1 = false
+  ex2 = false
+  ex3 = false
+  ex4 = false
+  ex5 = false
+
   constructor(public navCtrl: NavController, public navParams : NavParams) {
     this.exerciseNum = this.navParams.get('exercise')
     this.title = "Exercise " + this.exerciseNum
+    this.viewContent()
+  }
+
+  viewContent(){
+    switch(this.exerciseNum){
+      case '1' :
+      this.ex1 = true
+      break;
+      case '2' :
+      this.ex2 = true
+      break;
+      case '3' :
+      this.ex3 = true
+      break;
+      case '4' :
+      this.ex4 = true
+      break;
+      case '5' :
+      this.ex5 = true
+      break;
+      default :
+      console.log('Default')      
+      break;
+    }
   }
 
   ngOnInit(){
