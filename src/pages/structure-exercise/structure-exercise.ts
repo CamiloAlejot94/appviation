3641b229cc7b25b38af4a75b9f5cc31ec92ea831
AlxFL <alexander@grubbe.co>
2016-12-19 18:02:07 -0500

    Vocabulary and structure pages

---
+++ src/pages/structure-exercise/structure-exercise.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/*
+  Generated class for the StructureExercise page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-structure-exercise',
+  templateUrl: 'structure-exercise.html'
+})
+export class StructureExercise {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad StructureExercisePage');
+  }
+
+}
