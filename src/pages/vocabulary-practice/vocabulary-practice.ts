e74bc72dafd78947c88c6b5a5e41bacff4e276ab
AlxFL <alexander@grubbe.co>
2016-12-23 18:10:48 -0500

    vocabulary exercise 4 practice 1

--- src/pages/vocabulary-practice/vocabulary-practice.ts
+++ src/pages/vocabulary-practice/vocabulary-practice.ts
@@ -31,9 +31,14 @@ export class VocabularyPractice implements OnInit{
   btnDone=false
   minSec=false
 
+  ex1 = false
+  partsPlane = ["______", "______","______","______","______","______","______","______","______","______","______","______","______","______","______","______","______","______"]
+  ex4 = false
+
   words = [[], []]
-  answers = [["lack","of","readback"], ["Overlooked","relevant","information", "of", "the", "ATIS"]]
+  answers = [["lack","of","readback"], ["overlooked","relevant","information", "of", "the", "ATIS"]]
   myWords : any
+  myAnswer : any
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.practiceNum = this.navParams.get('numEx')
@@ -43,11 +48,18 @@ export class VocabularyPractice implements OnInit{
     if (this.exerciseNum==3 && this.practiceNum==1){
       this.btnMic=true
       this.minSec=true
-    }
-    else {
+    } else if(this.exerciseNum==1 && this.practiceNum==1){
+      this.ex1 = true    
+    } 
+    else if(this.exerciseNum==4 && this.practiceNum==1){
+      this.ex4 = true
       this.btnDone=true      
     }
+    else {
+      this.btnDone=true   
+    }
     this.myWords = this.answers[1];
+    this.myAnswer = this.words[1]
   }
 
   shwPause(){  
@@ -72,6 +84,17 @@ export class VocabularyPractice implements OnInit{
     this.btnPause=false
     this.btnPlay=false
 
+  }
+  pushAnswer(index, word){
+    document.getElementById(index+"-question").classList.toggle('opacityWords')
+    this.words[1].push({"index" : index, "word" : word})
+  }
+
+  popAnswer(word, index, i){
+    // document.getElementById(this.index+"-as").classList.toggle('opacityWords')
+    document.getElementById(i+"-answer").classList.toggle('displayWords')
+    document.getElementById(index+"-question").classList.toggle('opacityWords')
+    this.answers[1][index] = word
   }
 
   ngOnInit(){
