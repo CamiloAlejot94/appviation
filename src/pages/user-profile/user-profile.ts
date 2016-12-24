07cd89c3b79cfa13c8f63c3b71370d9c3ddbc0a8
Diana Carolina Alejo <Diana Carolina Alejo>
2016-12-24 01:44:08 -0500

    Profile page 100%

--- src/pages/user-profile/user-profile.ts
+++ src/pages/user-profile/user-profile.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
+
 
 
 @Component({
@@ -7,11 +8,142 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'user-profile.html'
 })
 export class UserProfilePage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+profName= 'Adam Sandler'
+profMail= 'Adam.Sandler@papcorn.com'
+profUser='AdamSandler'
+profPass: any
+profShield='Actor'
+showPass= '*****'
+  constructor(public navCtrl: NavController, public navParams: NavParams, public ctrl: AlertController )  {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad UserProfilePage');
   }
 
+
+  changeMail() {
+    let prompt = this.ctrl.create({
+      title: 'Change mail',
+      message: "Change your Email address",
+      inputs: [
+        {  
+          name: 'title',
+          placeholder: 'title'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            console.log(data)
+            this.profMail=data.title;               
+          }
+        }
+      ]
+    });
+    prompt.present();
+   
+  }
+
+  changeUser() {
+    let prompt = this.ctrl.create({
+      title: 'Change User',
+      message: "Change your User name",
+      inputs: [
+        {  
+          name: 'title',
+          placeholder: 'title'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            console.log(data)
+            this.profUser=data.title;               
+          }
+        }
+      ]
+    });
+    prompt.present();
+   
+  }
+
+  changePass() {
+    let prompt = this.ctrl.create({
+      title: 'Change Password',
+      message: "Change your Password for a new one",
+      inputs: [
+        {  
+          name: 'title',
+          placeholder: 'title'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            console.log(data)
+            this.profPass=data.title;
+            console.log(this.profPass)
+          }
+        }
+      ]
+    });
+    prompt.present();
+    
+   
+  }
+  
+  changeShield() {
+    let alert = this.ctrl.create();
+    alert.setTitle('Select your role');
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Pilot',
+      value: 'Pilot',
+      checked: true
+    });
+    alert.addInput({
+      type: 'radio',
+      label: 'ATC',
+      value: 'ATC',
+      checked: false
+    });
+
+    alert.addButton('Cancel');
+    alert.addButton({
+      text: 'OK',
+      handler: data => {
+        this.profShield=data
+      }
+    });
+    alert.present();
+  }
 }
+
+
+
+
+
+
+
